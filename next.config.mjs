53e3ad8c9a2e5a954eab37c9962707f78908ecae
min-38 <mindino03@gmail.com>
2025-02-21 22:50:56 +0900

    .env 파일 생성 및 사용
    - 백엔드 URL을 .env에 저장
    - dotenv 설치하여 .env 내용 적용
    - .gitignore에 .env 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+    },
+};
 
 export default nextConfig;
